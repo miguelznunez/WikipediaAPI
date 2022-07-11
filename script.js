9c1b8d0fd537bc560db25b5e4a1425d10187afa1
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2022-07-11 14:58:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,75 +1,73 @@
-const wikilinks = document.getElementsByClassName("wikilinks")[0];
-const searchThis = document.getElementById("input");
+const wikilinks = document.querySelector("#wikilinks"),
+searchThis = document.querySelector("#search"),
+searchButton = document.querySelector("#search-button");
 let imagesArray = [];
 
 searchThis.addEventListener("keydown", function(event){
-  if(event.key === 'Enter')
+  if(event.key === 'Enter'){
+    removeItems();
     request();
+  }
+})
+
+searchButton.addEventListener("click", function(){
+  removeItems();
+  request();
 })
 
 
 function request(){
 
-  removeItems();
-
   const url = 'https://en.wikipedia.org/w/api.php?action=query&origin=*&pilicense=any&prop=pageimages&pithumbsize=250&format=json&gsrlimit=100&generator=search&gsrsearch='+searchThis.value+'';
 
   fetch(url)
 
-  .then(response =>{
-    if(response.ok)
-      return response.json();
-    else
-      alert(response.status);
+  .then(response => {
+    if (response.status !== 200) throw Error(response.status);
+    else return response.json();
   })
 
   .then(data => {
-    console.log(data);
     loadLinks(data);
-  });  
+  })
+
+  .catch(error => { 
+    document.querySelector("#error").textContent = "Oops! Please try again!"
+  }); 
 }
 
 function loadLinks(data){
-  for (var pageId in data.query.pages) {
-    if (data.query.pages.hasOwnProperty(pageId)) {
-      var div = document.createElement('div');
-      div.className = 'link';
-      try{
-        div.innerHTML = '<img src='+data.query.pages[pageId].thumbnail.source+'>' + 
-                        '<h3>'+data.query.pages[pageId].title+'</h3>' +
-                        '<div class="middle">' +
-                          '<div>' +
-                            '<i class="fab fa-wikipedia-w"></i>' +
-                          '</div>' +
-                        '</div>';
-        wikilinks.appendChild(div);
-        imagesArray.push('https://en.wikipedia.org/?curid=' + pageId);
-      }
-      catch(err){
-       div.innerHTML = '<h3>'+data.query.pages[pageId].title+'</h3>' +
-                       '<div class="middle">' +
-                          '<div>' +
-                            '<i class="fab fa-wikipedia-w"></i>' +
-                          '</div>' +
-                        '</div>';
-        wikilinks.appendChild(div);
-        imagesArray.push('https://en.wikipedia.org/?curid=' + pageId);
-      }     
-              
-    }
-  }
-  addEvents();
-}
-
-function addEvents(){
-  for(let i = 0;i < 100;i++){
-    document.getElementsByClassName("link")[i].addEventListener('click', function(){
-      window.open(imagesArray[i]);
-    });
+  for (let pageId in data.query.pages) {
+    const link = document.createElement('div');
+    link.className = "link";
+    if(data.query.pages[pageId].thumbnail){
+      link.innerHTML =  `<a href="https://en.wikipedia.org/?curid=${pageId}" target="_blank">
+                          <img src="${data.query.pages[pageId].thumbnail.source}"> 
+                          <h3>${data.query.pages[pageId].title}</h3>
+                          <div class="middle">
+                            <div>
+                              <i class="fab fa-wikipedia-w"></i>
+                            </div>
+                          </div>
+                        </a>`;
+      wikilinks.appendChild(link);  
+    } else {
+      link.innerHTML = `<a href="https://en.wikipedia.org/?curid=${pageId}" target="_blank">
+                        <h3>${data.query.pages[pageId].title}</h3>
+                        <div class="middle">
+                          <div>
+                            <i class="fab fa-wikipedia-w"></i>
+                          </div>
+                        </div>
+                        </a>`;
+      wikilinks.appendChild(link);      
+    }      
+    
   }
 }
 
 function removeItems(){
+  document.querySelector("#error").textContent = "";
   wikilinks.innerHTML = '';
   imagesArray = [];
 }
